c94b5ac85fd15a3865a3ec19814e0ede6b26d19d
Jin An <jinsungan@gmail.com>
2026-08-24 10:43:42 -0400

    Update framework

--- framework/scripts/lecture-nav.js
+++ framework/scripts/lecture-nav.js
@@ -163,11 +163,23 @@
             };
         }
 
-        // Menu button - toggle menu
+        // FIXED: Menu button - toggle menu (CDN version uses toggle())
         if (menuBtn) {
             menuBtn.onclick = () => {
-                if (typeof Reveal !== 'undefined' && Reveal.getPlugin('menu')) {
-                    Reveal.getPlugin('menu').toggleMenu();
+                if (typeof Reveal !== 'undefined') {
+                    const menuPlugin = Reveal.getPlugin('menu');
+                    if (menuPlugin) {
+                        // CDN version uses toggle() instead of toggleMenu()
+                        if (typeof menuPlugin.toggle === 'function') {
+                            menuPlugin.toggle();
+                        } else if (typeof menuPlugin.toggleMenu === 'function') {
+                            menuPlugin.toggleMenu();
+                        } else {
+                            console.warn('Menu plugin loaded but no toggle method found');
+                        }
+                    } else {
+                        console.warn('Menu plugin not available');
+                    }
                 }
             };
         }
